f440014441897162ca280e359d722716269f46d4
imperfectcircle <fabio.angelici@outlook.it>
2021-11-03 20:02:20 +0100

    Aggiunto PiP

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
+/* eslint-disable no-alert */
+/* eslint-disable no-undef */
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
 
+// Prompt the user to select a media stream, pass to video element, then play
+const selectMediaStream = async () => {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        };
+    } catch (error) {
+        alert(
+            `Something gone wrong. Please try later or contact the website owner and report this error message: ${error}`,
+        );
+    }
+};
+
+button.addEventListener('click', async () => {
+    // Disable button
+    button.disable = true;
+    // Start PiP
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disable = false;
+});
+
+// On Load
+selectMediaStream();
